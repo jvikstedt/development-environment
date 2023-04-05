f7cbe10b72626388a1eda9547578bf3382ea5a21
Janne Vikstedt <vikstedt.janne@gmail.com>
2023-04-05 20:36:48 +0300

    New changes

---
+++ nvim/after/plugin/neotree.lua
@@ -0,0 +1,5 @@
+vim.keymap.set('n', '<leader>e', ":NvimTreeToggle<CR>", {})
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+require("nvim-tree").setup()
--- nvim/lua/custom/packer.lua
+++ nvim/lua/custom/packer.lua
@@ -8,11 +8,12 @@ return require('packer').startup(function(use)
         requires = { {'nvim-lua/plenary.nvim'} }
     }
 
+    use 'github/copilot.vim'
     use 'christoomey/vim-tmux-navigator'
     use 'lewis6991/gitsigns.nvim'
-    use 'preservim/nerdtree'
-    use 'Xuyuanp/nerdtree-git-plugin'
     use 'j-hui/fidget.nvim'
+    use 'nvim-tree/nvim-tree.lua'
+    use 'nvim-tree/nvim-web-devicons'
     use {
         'nvim-lualine/lualine.nvim',
         requires = { 'kyazdani42/nvim-web-devicons', opt = true }
